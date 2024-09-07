958220e575ac820b20128e2473d0a381019c8914
apurvabanka <appu.banka@gmail.com>
2024-09-06 23:59:21 -0400

    zremoving imports

--- src/main/java/net/bankatimes/dailyNews/controller/UserController.java
+++ src/main/java/net/bankatimes/dailyNews/controller/UserController.java
@@ -2,7 +2,6 @@ package net.bankatimes.dailyNews.controller;
 
 import java.util.List;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +9,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
