bd8131c7214a51fac0a01c2ce1b1d25ec460d700
apurvabanka <appu.banka@gmail.com>
2024-09-08 19:01:40 -0400

    Different tyes of logger

--- src/main/java/net/bankatimes/dailyNews/service/UserService.java
+++ src/main/java/net/bankatimes/dailyNews/service/UserService.java
@@ -7,20 +7,27 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 
 import org.bson.types.ObjectId;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 import net.bankatimes.dailyNews.entity.User;
 import net.bankatimes.dailyNews.respository.UserRepository;
 
 @Component
+@Slf4j //instead of creating instance we can use Bean
 public class UserService {
     
     @Autowired
     private UserRepository userRepository;
 
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    // private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public void saveUser(User user){
         user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -34,6 +41,11 @@ public class UserService {
     
 
     public List<User> getAll(){
+        log.info("Getting all users"); //changed from logger to log in-order to use the bean and not the instance
+        log.warn("This is warning");
+        log.error("This is error");
+        log.debug("This is debug");
+        log.trace("This is trace");
         return userRepository.findAll();
     }
 
